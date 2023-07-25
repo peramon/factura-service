ed1b8ca570b3d1b3f65d2f3b3ec57d57503dcb66
Paul Ramon <peramon98.18@gmail.com>
2023-07-25 00:24:12 -0500

    Creando la estructura del proyecto y la bd

---
+++ src/test/java/com/alquimiasoft/serviciofactura/ServicioFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquimiasoft.serviciofactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
