07d8cd376c6d41cfd018fcb6f2b09c51ebc80056
Paul Ramon <peramon98.18@gmail.com>
2023-07-25 00:32:44 -0500

    Agregando la entidad cliente

---
+++ src/main/java/com/alquimiasoft/serviciofactura/entity/Cliente.java
@@ -0,0 +1,25 @@
+package com.alquimiasoft.serviciofactura.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String tipoIdentificacion;
+    private String identificacion;
+    private String nombre;
+    private String correo;
+    private String numeroCelular;
+    private String direccion;
+
+}
